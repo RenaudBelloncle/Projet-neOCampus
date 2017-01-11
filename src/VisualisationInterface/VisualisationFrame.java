6a728aeea64545e960cb2848a24216e93ca97ad2
MeganeBige <amane-yukio-sama@hotmail.fr>
2017-01-11 15:26:16 +0100

    Visualisation Mainframe améliorations

--- src/VisualisationInterface/VisualisationFrame.java
+++ src/VisualisationInterface/VisualisationFrame.java
@@ -9,21 +9,30 @@ import java.awt.event.ActionListener;
 
 public class VisualisationFrame extends JFrame implements TreeSelectionListener, ActionListener {
 
-    private JPanel sensor_panel;
     private JTabbedPane tabbed_panel;
     private JScrollPane scroll_panel;
+    private JSplitPane main_split_panel;
     private JSplitPane split_panel;
 
-    private JButton close;
-    private JLabel title;
+    private JTextArea dialog_area;
+
+    private JMenuBar menuBar;
+    private JMenu menu;
+    private JMenu menu_data;
+    private JMenuItem connection;
+    private JMenuItem refresh;
+    private JMenuItem signIn;
+    private JMenuItem signOut;
 
     private JTree tree = new JTree();
 
-    private int nbTab=0;
+    private int nb_Sensor = 0;
+    private JLabel status;
+    private JLabel nb_Sensor_label;
 
     public VisualisationFrame(){
         super("Visualisation des capteurs");
-        setSize(400, 450);
+        setSize(850, 700);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         initialize();
         place();
@@ -33,23 +42,53 @@ public class VisualisationFrame extends JFrame implements TreeSelectionListener,
     }
 
     private void initialize() {
-        close = new JButton("X");
-        title = new JLabel("Onglet ");
+        menuBar = new JMenuBar();
+        menu = new JMenu("Menu");
+        menu_data = new JMenu("Données");
+        signIn = new JMenuItem("Inscription aux capteurs");
+        refresh = new JMenuItem("Actualiser");
+        signOut = new JMenuItem("Désinscription aux capteurs");
+        connection = new JMenuItem("Connexion au serveur");
+        status = new JLabel("   Status : Déconnecté    ");
+        nb_Sensor_label = new JLabel("Nb capteurs : "+nb_Sensor);
         tabbed_panel = new JTabbedPane(SwingConstants.TOP);
         scroll_panel = new JScrollPane(tree, ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
         split_panel = new JSplitPane(JSplitPane.VERTICAL_SPLIT, scroll_panel, tabbed_panel);
-        sensor_panel = new JPanel();
+        dialog_area = new JTextArea(100, 20);
+        main_split_panel = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, dialog_area, split_panel);
     }
 
     private void place() {
         Container content = getContentPane();
-        sensor_panel.add(close);
-        content.add(split_panel);
+        menu.add(connection);
+        menu.add(signIn);
+        menu.add(signOut);
+        menuBar.add(menu);
+        menu_data.add(refresh);
+        menuBar.add(menu_data);
+        menuBar.add(status);
+        menuBar.add(nb_Sensor_label);
+        setJMenuBar(menuBar);
+        content.add(main_split_panel);
+    }
+
+    public void switchText() {
+        if (connection.getText() == "Connexion au serveur") {
+            connection.setText("Déconnexion du serveur");
+            status.setText("   Status : Connecté      ");
+        }
+        else {
+            connection.setText("Connexion au serveur");
+            status.setText("   Status : Déconnecté    ");
+        }
     }
 
     private void setListener() {
         tree.addTreeSelectionListener(this);
-        close.addActionListener(this);
+        signOut.addActionListener(this);
+        signIn.addActionListener(this);
+        connection.addActionListener(this);
+        refresh.addActionListener(this);
     }
 
     @Override
@@ -57,13 +96,28 @@ public class VisualisationFrame extends JFrame implements TreeSelectionListener,
         String node = e.getNewLeadSelectionPath().getLastPathComponent().toString();
         if (node == "") {
         } else {
-            nbTab++;
             tabbed_panel.add(node, new JPanel());
         }
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == connection) {
+            if (connection.getText() == "Connexion au serveur") new ConnectionFrame();
+            else {
 
+            }
+            switchText(); // à placer ailleurs
+        }
+        if (e.getSource() == signIn) { // gestion erreur si déjà inscrit à tous les capteurs ?
+            nb_Sensor++;
+            nb_Sensor_label.setText("Nb capteurs : "+nb_Sensor);
+        }
+        if (e.getSource() == signOut) {
+            if (nb_Sensor > 0) {
+                nb_Sensor--;
+                nb_Sensor_label.setText("Nb capteurs : "+nb_Sensor);
+            } // sinon erreur
+        }
     }
 }
